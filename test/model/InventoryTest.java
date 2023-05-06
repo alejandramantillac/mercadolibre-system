f15a5968682d0e04b8d935f2f0aef2f857971300
Pablo Fernando Pineda <pinedapablo6718@gmail.com>
2023-05-06 16:52:32 -0500

    OrderSearcher tests implementation and more tests in inventory

--- test/model/InventoryTest.java
+++ test/model/InventoryTest.java
@@ -1,12 +1,14 @@
 package model;
 
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertEquals;
 
 public class InventoryTest {
 
@@ -17,8 +19,23 @@ public class InventoryTest {
         inventory.getProducts().clear();
         inventory.addProduct(new Product("Product A", "Description A", ProductCategory.BOOKS, 22.0, 2, 2));
         inventory.addProduct(new Product("Product B", "Description B", ProductCategory.BEAUTY_AND_PERSONAL_CARE, 56.0, 6, 3));
-        inventory.saveProducts();
+        //inventory.saveProducts();
     }
+
+
+    public void setupStage2() {
+        ArrayList<Product> products1 = new ArrayList<>();
+        products1.add(new Product("Product 1", "the description 1", ProductCategory.STATIONERY, 10.0, 5, 1));
+        inventory = new Inventory();
+        inventory.getOrders().clear();
+        inventory.addOrder(new Order("Alejandra", products1, 50, "06/06/2006"));
+
+        ArrayList<Product> products2 = new ArrayList<>();
+        products2.add(new Product("Product 2", "the description 2", ProductCategory.ELECTRONICS, 15.0, 7, 0));
+        inventory.addOrder(new Order("Pablo", products2, 105, "07/07/2007"));
+        inventory.saveOrders();
+    }
+
 
     @Test
     public void testAddProduct() {
@@ -28,11 +45,32 @@ public class InventoryTest {
     }
 
     @Test
+    public void testAddProductMultiple(){
+        setupStage1();
+
+        inventory.addProduct(new Product("Product C", "Description C", ProductCategory.TOYS_AND_GAMES, 108, 1, 10));
+        inventory.addProduct(new Product("Product D", "Description D", ProductCategory.CLOTHING_ACCESSORIES, 20, 7, 13));
+
+        assertEquals(4, inventory.getProducts().size());
+        assertEquals("Product D",inventory.getProducts().get(3).getName());
+    }
+
+
+    @Test
     public void testRemoveProduct() {
         setupStage1();
         inventory.removeProduct(inventory.getProducts().get(0));
         assertEquals(1, inventory.getProducts().size());
     }
+
+    @Test
+    public void testRemoveProductMultiple() {
+        setupStage1();
+        inventory.removeProduct(inventory.getProducts().get(0));
+        inventory.removeProduct(inventory.getProducts().get(0));
+        assertEquals(0, inventory.getProducts().size());
+    }
+
 
     @Test
     public void testSaveLoadProducts() throws IOException {
@@ -41,18 +79,60 @@ public class InventoryTest {
         inventory.loadProducts();
 
         List<Product> loadedProducts = new Inventory().getProducts();
-        Assertions.assertEquals(2, loadedProducts.size());
+        assertEquals(2, loadedProducts.size());
 
-        Assertions.assertEquals("Product A", loadedProducts.get(0).getName());
-        Assertions.assertEquals(22, loadedProducts.get(0).getPrice());
-        Assertions.assertEquals(2, loadedProducts.get(0).getQuantity());
-        Assertions.assertEquals(2, loadedProducts.get(0).getTimesPurchased());
-        Assertions.assertEquals("Product B", loadedProducts.get(1).getName());
-        Assertions.assertEquals(56, loadedProducts.get(1).getPrice());
-        Assertions.assertEquals(6, loadedProducts.get(1).getQuantity());
-        Assertions.assertEquals(3, loadedProducts.get(1).getTimesPurchased());
+        assertEquals("Product A", loadedProducts.get(0).getName());
+        assertEquals(22, loadedProducts.get(0).getPrice());
+        assertEquals(2, loadedProducts.get(0).getQuantity());
+        assertEquals(2, loadedProducts.get(0).getTimesPurchased());
+        assertEquals("Product B", loadedProducts.get(1).getName());
+        assertEquals(56, loadedProducts.get(1).getPrice());
+        assertEquals(6, loadedProducts.get(1).getQuantity());
+        assertEquals(3, loadedProducts.get(1).getTimesPurchased());
     }
 
+    @Test
+    public void testAddOrder() {
+        setupStage2();
+        assertEquals(2, inventory.getOrders().size());
+        assertEquals("Alejandra", inventory.getOrders().get(0).getCustomerName());
+    }
 
+    @Test
+    public void testMultipleOrder(){
+        setupStage2();
+        ArrayList<Product> productss = new ArrayList<>();
+        productss.add(new Product("Product 3", "the description 3", ProductCategory.STATIONERY, 30.0, 1, 30));
+        inventory = new Inventory();
+        inventory.addOrder(new Order("Mantilla", productss, 30, "02/12/2002"));
+
+        ArrayList<Product> products2 = new ArrayList<>();
+        productss.add(new Product("Product 4", "the description 4", ProductCategory.SPORTS, 14.0, 14, 1));
+        inventory.addOrder(new Order("Pineda", productss, 350, "09/08/2006"));
+        assertEquals(4, inventory.getOrders().size());
+    }
+
+    @Test
+    public void testRemoveOrder() {
+        setupStage2();
+        inventory.removeOrder(inventory.getOrders().get(0));
+        assertEquals(1, inventory.getOrders().size());
+    }
+
+    @Test
+    public void SaveLoadOrders() {
+        setupStage2();
+
+        inventory.loadOrders();
+
+        List<Order> loadedOrders = new inventory.getOrders();
+
+        assertEquals("Alejandra", loadedOrders.get(0).getCustomerName());
+        assertEquals(50, loadedOrders.get(0).getTotal());
+        assertEquals("06/06/2006", loadedOrders.get(0).getDate());
+        assertEquals("Pablo", loadedOrders.get(1).getCustomerName());
+        assertEquals(105, loadedOrders.get(1).getTotal());
+        assertEquals("07/07/2007", loadedOrders.get(1).getDate());
+    }
 
 }
--- test/model/OrderSearcherTest.java
+++ test/model/OrderSearcherTest.java
@@ -1,4 +1,85 @@
 package model;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import exceptions.ProductNotFoundException;
+import org.testng.annotations.Test;
+
 public class OrderSearcherTest {
+
+    private OrderSearcher searcher;
+
+
+    public void setupStage1(){
+        ArrayList<Product> products1 = new ArrayList<>();
+        products1.add(new Product("product1", "description1", ProductCategory.SPORTS, 10.0, 15, 9));
+
+        ArrayList<Product> products2 = new ArrayList<>();
+        products2.add(new Product("product2", "description2", ProductCategory.STATIONERY, 5.0, 12, 6));
+
+        Order order1 = new Order("customer1", products1, 20.5, "18/07/2020");
+        Order order2 = new Order("customer2", products2, 50.7, "31/12/2022");
+
+        ArrayList<Order> orders = new ArrayList<>();
+        orders.add(order1);
+        orders.add(order2);
+        searcher= new OrderSearcher(orders);
+    }
+
+    @Test
+    public void testSearchByCustomerNameOrder() {
+        setupStage1();
+
+        List<Order>results = searcher.searchByCustomerName("customer1");
+        assertEquals(1, results.size());
+        assertEquals("customer1", results.get(0).getCustomerName());
+    }
+
+    @Test
+    public void testSearchByCustomerNameOrderNonExistent() {
+        setupStage1();
+
+        try{
+            List<Order>results = searcher.searchByCustomerName("customer1");
+            searcher.searchByCustomerName("customer1");
+        }catch (OrderNotFoundException e){
+            assertNotNull(e.getMessage());
+            assertEquals("No orders found with that criteria.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSearchByTotalOrder(){
+        setupStage1();
+
+        List<Order>results = searcher.searchOrdersByTotal(20.5);
+        assertEquals(1, results.size());
+        assertEquals("customer1", results.get(0).getCustomerName());
+    }
+
+    @Test
+    public void testSearchByTotalOrderNonExistent(){
+        setupStage1();
+
+        try{
+            List<Order>results = searcher.searchOrdersByTotal(40);
+            searcher.searchOrdersByTotal(40);
+        }catch (OrderNotFoundException e){
+            assertNotNull(e.getMessage());
+            assertEquals("No orders found with that criteria.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSearchByDateOrder(){
+        setupStage1();
+
+        List<Order>results = searcher.searchOrdersByDate("18/07/2020");
+        assertEquals(1, results.size());
+        assertEquals("customer1", results.get(0).getCustomerName());
+    }
+
 }
